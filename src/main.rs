3663fdbf3ada69333b53fd1704e34b0d9406621b
Nelson <nelson.chamberlain7@gmail.com>
2026-05-01 22:36:25 -0700

    added date-time charts to show when highest & lowest temperatures happened

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
 use colorize::AnsiColor;
-use std::process::Command;
 use execute::Execute;
 use sqlx::{mysql::{MySqlPoolOptions, MySqlRow}, MySql, Pool, Row};
 use dotenvy::dotenv;
+use tokio::sync::RwLockMappedWriteGuard;
 use std::env;
-use std::io::{self, Write};
 use std::sync::OnceLock;
+use std::process::Command;
+use std::io::{self, Write};
+use std::{collections::HashMap, error::Error};
 use plotters::prelude::*;
 use plotters::coord::Shift;
+use chrono::{NaiveDate, Months};
+//use fahrenheit_to_celsius::f_to_c;
 
 //use static for db connection pooling to create a single pool that can be shared across 
 //multiple functions without having to pass it around as an argument. 
@@ -62,12 +66,13 @@ async fn get_user_choice() -> Result<(), sqlx::Error>{
         "List all cities",
         "Generate BAR Charts by CITY",
         "Generate LINE Charts by CITY",
+        "Generate Wide Date-Temp Charts by CITY - UNDER CONSTRUCTION",
         "Generate Videos from Charts",
         "Exit",
     ];
 
     loop {
-        let prompt_message = "Please select a database action".blue();
+        let prompt_message = "Please select a charting action".blue();
         let select = inquire::Select::new(&prompt_message, choices.clone())
             .prompt()
             .expect("Failed to select a database action");
@@ -84,10 +89,14 @@ async fn get_user_choice() -> Result<(), sqlx::Error>{
             //println!("Generate LINE Charts by CITY - UNDER CONSTRUCTION");
             generate_line_charts_by_city().await.expect("Failed to generate line charts by city");
         }
+          else if select == "Generate Wide Date-Temp Charts by CITY - UNDER CONSTRUCTION" {
+            println!("Generate Wide Date-Temp Charts by CITY - UNDER CONSTRUCTION");
+            generate_date_time_charts_by_city().await.expect("Failed to generate line charts by city");
+        }
           else if select == "Generate Videos from Charts" {
             generate_videos_by_city().await.expect("Failed to generate videos from charts");
-        }  else
-        if select == "Exit" {
+        }  
+          else if select == "Exit" {
             println!("Exiting the program. Goodbye!");
             break;
         }
@@ -792,9 +801,125 @@ fn generate_videos(city: &str, start_year: i32, fps: i32) -> Result<(), Box<dyn 
 
     Ok(())
 }
+async fn generate_date_time_charts_by_city()  -> Result<(), sqlx::Error> {
+    let selected_cities = select_cities("Please select the cities to GENERATE date-time charts".to_string()).await;
+    for the_city in selected_cities {
+        println!("Generating date-time charts for city of {0}", the_city.clone().red());
+        generate_date_time_charts(&the_city).await.expect("Failed to generate date-time charts");    
+    }
+    Ok(())
+}
+async fn generate_date_time_charts(the_city: &str) -> Result<(), sqlx::Error> {
+    let city  = String::from(the_city);
+    for chart_type in 1..=4 { //1=high-Max, 2=hig-min, 3=low-max, 4=low-min
+        make_date_time_charts(&city, chart_type).await?;    
+    }
+    Ok(())
+}
+async fn make_date_time_charts(city: &str, chart_type: i32) -> Result<(), sqlx::Error> {
+    let mut chart_title = String::new();
+    let mut out_file_name = String::new();
+    let mut query_string = String::new();//format!("SELECT tdate, tmax, tmin FROM {} ORDER BY tdate", city);
+    match chart_type {
+        1 => {println!("Generating High-Max charts for {city}");
+                chart_title = format!("{}: 100 Hottest Daytime Temperatures", city);
+                query_string = format!("SELECT tdate, tmax FROM {} where tmax is not null ORDER BY tmax DESC, tdate ASC LIMIT 500", city);
+                out_file_name = format!("date_charts/{}_high_max.svg", city);
+            },
+        2 => {println!("Generating High-Min charts for {city}");
+                chart_title = format!("{}: 100 Coolest Daytime Temperatures", city);
+                query_string = format!("SELECT tdate, tmax FROM {} where tmax is not null ORDER BY tmax ASC, tdate ASC LIMIT 400", city);
+                out_file_name = format!("date_charts/{}_high_min.svg", city);
+            },
+        3 => {println!("Generating Low-Max charts for {city}");
+                chart_title = format!("{}: 100 Coldest Nighttime Temperatures", city);
+                query_string = format!("SELECT tdate, tmin FROM {} where tmin is not null ORDER BY tmin ASC, tdate ASC LIMIT 300", city);
+                out_file_name = format!("date_charts/{}_low_max.svg", city);
+            },
+        4 => {println!("Generating Low-Min charts for {city}");
+                chart_title = format!("{}: 100 Warmest Nighttime Temperatures", city);
+                query_string = format!("SELECT tdate, tmin FROM {} where tmin is not null ORDER BY tmin DESC, tdate ASC LIMIT 200", city);
+                out_file_name = format!("date_charts/{}_low_min.svg", city);
+            },
+        _ => println!("Unknown chart type"),
+    }
+    let rows: Vec<sqlx::mysql::MySqlRow> = sqlx::query(&query_string)
+        .fetch_all(DB_POOL.get().expect("Database pool not initialized"))
+        .await?;
+    let dup_dates: HashMap<&str, (f64, i32)> = find_dup_dates(&rows); //use this hashmap later
+
+    // find highest & lowest Temps from rows to set the y axis range
+    let highest_temp: f64= rows.iter().map(|row| row.get::<i32, _>(1)).max().unwrap_or(0) as f64 + 1.0;
+    let lowest_temp: f64 = rows.iter().map(|row| row.get::<i32, _>(1)).min().unwrap_or(0) as f64 - 1.0;
+    // find the earliest and latest dates to set x axis range
+    let earliest_date_str = rows.iter().map(|row| row.get::<&str, _>(0)).min().unwrap_or("1800-01-01");
+    let latest_date_str = rows.iter().map(|row| row.get::<&str, _>(0)).max().unwrap_or("2070-01-01");
+    let earliest_date = NaiveDate::parse_from_str(earliest_date_str, "%Y-%m-%d").unwrap_or(NaiveDate::from_ymd_opt(1870, 1, 1).unwrap()) - Months::new(11); // subtract 11 month to give some padding on the left side of the chart
+    let latest_date = NaiveDate::parse_from_str(latest_date_str, "%Y-%m-%d").unwrap_or(NaiveDate::from_ymd_opt(2070, 1, 1).unwrap()) + Months::new(11); // add 11 month to give some padding on the right side of the chart
+
+    let root = SVGBackend::new(&out_file_name, (1440, 480)).into_drawing_area();
+    let _ = root.fill(&WHITE);
+    let mut chart = ChartBuilder::on(&root)
+        .margin(10)
+        .caption(chart_title, ("sans-serif", 40),)
+        .set_label_area_size(LabelAreaPosition::Left, 60)
+        .set_label_area_size(LabelAreaPosition::Right, 60)
+        .set_label_area_size(LabelAreaPosition::Bottom, 40)
+        .build_cartesian_2d((earliest_date..latest_date).monthly(),lowest_temp..highest_temp,
+        ).unwrap()
+        .set_secondary_coord((earliest_date..latest_date).monthly(), f_to_c(lowest_temp)..f_to_c(highest_temp),
+        );
+
+    chart
+        .configure_mesh()
+        .disable_x_mesh()
+        //.disable_y_mesh()
+        .x_labels(30)
+        .max_light_lines(4)
+        .y_desc("Average Temp (F)")
+        .draw().unwrap();
+    chart
+        .configure_secondary_axes()
+        .y_desc("Average Temp (C)")
+        .draw().unwrap();
+
+    chart.draw_series(
+        rows.iter()
+            .map(|row: &sqlx::mysql::MySqlRow| {
+                let date_str: &str = row.get(0);
+                let temp: i32 = row.get(1);
+                Circle::new(
+                (NaiveDate::parse_from_str(date_str, "%Y-%m-%d").unwrap(), temp as f64),
+                     2, BLUE.filled())})
+            ).expect("Error drawing series");
+
+    // find dup dates and update chart
+    for item in &dup_dates {
+        println!("Duplicate date found: {} for temp: {} count: {}", item.0, item.1.0, item.1.1);
+    }
+    chart.draw_series(
+        dup_dates.iter()
+            .map(|item| {
+                let date_str = item.0;
+                let temp = item.1.0;
+                let count = item.1.1;
+                TriangleMarker::new(
+                    (NaiveDate::parse_from_str(date_str, "%Y-%m-%d").unwrap(), temp),
+                    4,
+                    RED.filled(),
+                )
+            })
+    ).expect("Error drawing duplicate date markers");
+
+    root.present().expect("Unable to write result to file, please make sure 'date_charts' folder exists under current dir");
+    println!("Result has been saved to {}", out_file_name);
+
+
+    Ok(())
+}
 
 async fn generate_line_charts_by_city() -> Result<(), sqlx::Error> {
-    let selected_cities = select_cities("Please select the cities to GENERATE videos".to_string()).await;
+    let selected_cities = select_cities("Please select the cities to GENERATE line charts".to_string()).await;
     for the_city in selected_cities {
         println!("Generating line charts for city of {0}", the_city.clone().red());
         generate_line_charts(&the_city).await?;    
@@ -812,8 +937,8 @@ async fn generate_line_charts(the_city: &str) -> Result<(), sqlx::Error> {
         Err(e) =>  {city_low = 0; city_high = 0; eprintln!("Error getting City min max: {}",e)}
     }  
     //let mut first_year: i32 = 0; 
-    let first_year: i32 = get_1st_year(&the_city).await;
-    let last_year: i32 = get_end_year(&the_city).await;
+    let first_year: i32 = get_1st_year(city).await;
+    let last_year: i32 = get_end_year(city).await;
 
     make_monthly_charts(city, first_year, last_year, city_low, city_high).await?;
     make_fortly_charts(city, first_year, last_year, city_low, city_high).await?;
@@ -1120,4 +1245,43 @@ fn split_segments(data: Vec<(i32, i32)>) -> Vec<Vec<(i32, i32)>>
         segments.push(current_segment);
     }
     segments
+}
+fn f_to_c(num: f64) -> f64 {
+    let result = (num - 32.0) * 5.0 / 9.0;
+    println!("\nC = {}", result);
+    result
+}
+
+fn find_dup_dates(rows: &Vec<sqlx::mysql::MySqlRow>) -> HashMap<&str, (f64, i32)> {
+    let mut dup_dates: HashMap<&str, (f64, i32)> = HashMap::new();
+    let mut tdate = "1800-01-01";
+    let mut count = 1;
+
+    for row in 0..rows.len()-1 {
+        let current_row = &rows[row]; //should be a tdate and a temp (either tmax or tmin depending on the chart)
+        let next_row = &rows[row + 1];
+        let current_temp: i32 = current_row.get(1);
+        let next_temp: i32 = next_row.get(1);
+        //let next_date: &str = next_row.get(0);
+        if current_temp == next_temp { // ONLY process days with matching temps
+            let days_apart = dates_diff_in_days(current_row.get(0), next_row.get(0));
+            if days_apart < 90 {
+                count += 1;
+                if tdate == "1800-01-01" { //this check preserves original tdate with subsequent readings that will appear in the same dot-space
+                    tdate = current_row.get(0);
+                }
+                dup_dates.insert(tdate, (current_temp as f64, count));
+                //println!("Temp = {}: Dates {} and {} are {} days apart. Count: {}. tdate: {}", current_temp, tdate, next_date, days_apart, count, tdate);
+            } else {
+                count = 1;
+                tdate = "1800-01-01";
+            }
+        }
+    }
+    dup_dates
+}
+fn dates_diff_in_days(a: &str, b: &str) -> i64 {
+    let a_date = NaiveDate::parse_from_str(a, "%Y-%m-%d").unwrap();
+    let b_date = NaiveDate::parse_from_str(b, "%Y-%m-%d").unwrap();
+    (a_date - b_date).num_days().abs()
 }
